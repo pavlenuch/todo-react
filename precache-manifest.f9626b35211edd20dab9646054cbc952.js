bc83af99e8fa5a538b966caa9bd9e67443671d5f
Павленко Александр Алексеевич <pavlenko@galaktika.local>
2021-01-13 16:07:07 +0300

    Updates

---
+++ precache-manifest.f9626b35211edd20dab9646054cbc952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658b79e1437bebdf88987d2ead834c91",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "0ff3e3cf76aa19cac0af",
+    "url": "/todo-react/static/css/main.dbfe7f4b.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/todo-react/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff3e3cf76aa19cac0af",
+    "url": "/todo-react/static/js/main.6830a2d5.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
